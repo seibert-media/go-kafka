005915defce9e597f1c655c3a249c635c1cbd313
Benjamin Borbe <bborbe@rocketnews.de>
2019-07-16 15:07:58 +0200

    add data error

--- consumer/message_handler_sentry.go
+++ consumer/message_handler_sentry.go
@@ -17,11 +17,19 @@ func SendErrorsToSentry(messageHandler MessageHandler) MessageHandler {
 	return MessageHandlerFunc(func(ctx context.Context, msg *sarama.ConsumerMessage) error {
 		if err := messageHandler.ConsumeMessage(ctx, msg); err != nil {
 			glog.Warningf("consume message %d in topic %s failed: %v", msg.Offset, msg.Topic, err)
-			raven.CaptureErrorAndWait(err, map[string]string{
-				"topic":     msg.Topic,
-				"partition": fmt.Sprintf("%d", msg.Partition),
-				"offset":    fmt.Sprintf("%d", msg.Offset),
-			})
+			raven.CaptureErrorAndWait(
+				err,
+				DataFromError(
+					AddDataToError(
+						err,
+						map[string]string{
+							"topic":     msg.Topic,
+							"partition": fmt.Sprintf("%d", msg.Partition),
+							"offset":    fmt.Sprintf("%d", msg.Offset),
+						},
+					),
+				),
+			)
 		}
 		return nil
 	})
